a50a752b57cbf76649ce89f7d5d1c0233f2c505d
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-12 12:50:53 +0100

    Adding action types and creators

---
+++ src/features/mates/reducer/action.creator.ts
@@ -0,0 +1,8 @@
+import { createAction } from "@reduxjs/toolkit";
+import { Mate } from "../types/mate";
+import { actionTypes } from "./action.types";
+
+export const loadActionCreator = createAction<Array<Mate>>(actionTypes.load);
+export const addActionCreator = createAction<Mate>(actionTypes.add);
+export const updateActionCreator = createAction<Mate>(actionTypes.update);
+export const deleteActionCreator = createAction<Mate["id"]>(actionTypes.delete);
---
+++ src/features/mates/reducer/action.types.ts
@@ -0,0 +1,6 @@
+export const actionTypes = {
+    load: "mate@load",
+    add: "mates@add",
+    update: "mates@update",
+    delete: "mates@delete",
+};
