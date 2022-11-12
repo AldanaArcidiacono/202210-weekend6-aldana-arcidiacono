3991e3cf202f68e063b32ce1e9ef4bcfe76ec379
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-12 13:09:44 +0100

    Adding config to store

--- src/app/redux-toolkit/store.ts
+++ src/app/redux-toolkit/store.ts
@@ -1,17 +1,11 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { mateReducer } from "../../features/mates/reducer/reducer";
 
-export const store = configureStore({
+export const appStore = configureStore({
     reducer: {
         mate: mateReducer,
     },
 });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    Action<string>
->;
+export type rootStore = typeof appStore;
+export type rootState = ReturnType<typeof appStore.getState>;
