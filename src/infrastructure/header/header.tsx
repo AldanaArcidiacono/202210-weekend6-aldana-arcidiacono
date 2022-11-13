807f4e2f05868779d5b0cab85ef459984e3bd4fd
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-13 20:34:11 +0100

    Adding header css

--- src/infrastructure/header/header.tsx
+++ src/infrastructure/header/header.tsx
@@ -1,9 +1,10 @@
 import { Menu } from "../menu/menu";
+import styles from "./header.module.css";
 
 export function Header() {
     const title = "Che Mate🧉";
     return (
-        <header>
+        <header className={styles.header}>
             <h1>{title}</h1>
             <Menu></Menu>
         </header>
