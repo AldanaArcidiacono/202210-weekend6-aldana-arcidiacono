a80c18c25ba84d65c98f4fa72612e231e76da061
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-13 18:55:22 +0100

    Adding dessert page

--- src/features/dessert/page/dessert.page.tsx
+++ src/features/dessert/page/dessert.page.tsx
@@ -1,7 +1,12 @@
+import { DessertList } from "../components/dessert.list/dessert.list";
+import { useDessert } from "../hooks/usedessert";
+
 function DessertPage() {
+    const { desserts } = useDessert();
     return (
         <main>
             <h2>Sección de Postres</h2>
+            <DessertList item={desserts}></DessertList>
         </main>
     );
 }
