0f2b863aa4d1e43920c55f282fba52e4a3ec4753
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-13 20:34:54 +0100

    Trying css for modules

--- src/features/mates/components/mate.item/mate.item.tsx
+++ src/features/mates/components/mate.item/mate.item.tsx
@@ -4,7 +4,7 @@ export function MateItem({ item }: { item: Mate }) {
     return (
         <>
             <p>{item.title}</p>
-            <img src={item.img} alt={"Image of" + item.title} />
+            <img src={item.img} alt={"Image of" + item.title} height="200" />
             <p>{item.price}</p>
         </>
     );
